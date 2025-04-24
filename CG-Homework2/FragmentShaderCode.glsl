1ab377eac53edc1d0d0a9cb6711a75489c21b491
hxj616 <1213963891hxj@gmail.com>
2025-04-24 13:26:10 +0800

    Revert "完成顶点着色器和片段着色器代码"
    This reverts commit af30309ff69f4687aeddbe34b1ef41153fa00459.

--- CG-Homework2/FragmentShaderCode.glsl
+++ CG-Homework2/FragmentShaderCode.glsl
@@ -1,24 +1,6 @@
 #version 430
 
-in layout(location=0) vec3 position;
-in layout(location=1) vec3 normal;
-in layout(location=2) vec2 texCoord;
-
-uniform mat4 modelTransformMatrix;
-uniform mat4 viewMatrix;
-uniform mat4 projectionMatrix;
-
-out vec3 normal;
-out vec3 fragPosition;
-out vec2 texCoords;
-
 void main()
 {
-	vec4 v = vec4(position, 1.0);
-	fragPosition = vec3(modelTransformMatrix * v);
-	normal = mat3(transpose(inverse(modelTransformMatrix))) * normal;
-	texCoords = texCoord;
-
-	vec4 out_position = projectionMatrix * viewMatrix * modelTransformMatrix * v;
-	gl_Position = out_position;	
+	
 }
